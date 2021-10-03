251db9a313008f790ba1250b59d7aee23470a581
unknown <friso.van.reede@student.nhlstenden.com>
2021-10-03 23:07:17 +0200

    edit folder

---
+++ ArcadeGameProject/ArcadeGameProject/Enemies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace ArcadeGameProject
+{
+    class Enemies
+    {
+        //alle, tot nu toe, variabelen voor enemies
+        // hoe deze class nu wordt gebruikt is als een extra opslag voor variabelen voor elke enemy op het beeld, dus er wordt voor elke enemy een aparte class met de zelfde naam en variabelenen mogelijkheden gemaakt, alleen de nummers ahter de variabelen kunnen verschillen.
+        public Rectangle rectangle;
+        public bool ToLeft;
+        public bool ToRight;
+        public Enemytype enemyType;
+        public int InWall;
+        public int OutWall;
+        public int score;
+        public side WhichSide;
+        //public int HitTime;
+        
+    }
+}
+//enums zijn een soort van status mogelijkheid die gebruikt kan worden in if statements om te kijken of iets voldoet of niet, heel handig
+public enum Enemytype {Enemy1, Enemy2 }
+public enum side {left, right}
---
+++ ArcadeGameProject/ArcadeGameProject/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcadeGameProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickPlay(object sender, RoutedEventArgs e)
+        {
+            //opent de Gamewindow
+            GameWindow GW = new GameWindow();
+            GW.Visibility = Visibility.Visible;
+        }
+
+        private void OnClickExit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
