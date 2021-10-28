38b94f32d6b11fe7e7ce2cd92c32597f76a4f73f
Gago Khachatryan <gago.khachatryan@student.nhlstenden.com>
2021-10-28 19:36:51 +0200

    git merge

---
+++ ArcadeGameProject/ArcadeGameProject/AdminWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArcadeGameProject
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static string password;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(txtUser.Text))
+            {
+                string password = txtUser.Text;
+                MainWindow.checkpassword = password;
+                this.Close();
+            }
+            else
+                MessageBox.Show("Voer een wachtwoord in");
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
