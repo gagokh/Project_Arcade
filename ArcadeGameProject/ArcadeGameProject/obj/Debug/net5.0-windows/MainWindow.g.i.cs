d8fd25bc00fba97b61f89b30aa95543c1166ebcc
Patrick-Kuipers <patrick_k1994@hotmail.nl>
2021-10-28 14:08:29 +0200

    niks aangepast

--- ArcadeGameProject/ArcadeGameProject/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ ArcadeGameProject/ArcadeGameProject/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace ArcadeGameProject {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ArcadeGameProject;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ArcadeGameProject;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
